9f2412dc5776640106c431324ddb28fdded6cd84
Jennifer Machado <dev.jenni@outlook.com>
2023-10-23 18:48:53 +0100

    modificado

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = `https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json`;
+    const url = `https://raw.githubusercontent.com/ihaunt/Portfolio/main/assets/data/profile.json?token=GHSAT0AAAAAACIHZZ5PBJ6ZTQP4QSTJ4PJAZJWWAVA`;
     const fetching = await fetch(url)
     return await fetching.json()
 }// conexão com API
